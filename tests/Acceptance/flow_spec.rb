77a45156bbe00eb713daf4ea7faae7360e8abde4
NicoPaez <nicolas.paez@kleer.la>
2014-03-25 13:16:25 +0100

    Acceptance tests updated

--- tests/Acceptance/flow_spec.rb
+++ tests/Acceptance/flow_spec.rb
@@ -20,8 +20,6 @@ describe "Octopush end to end pipeline flow" do
     url = octopush_url + "/jobs/#{job_id}/status"
     json_response = Octopush.get(url)
     json_response.body.should include 'QUEUED'
-
-    Octopush.get(octopush_url + "/run");
     
     # getStatus   
     sleep 50
@@ -29,15 +27,11 @@ describe "Octopush end to end pipeline flow" do
     json_response = Octopush.get(url)
     json_response.body.should include 'DEPLOYING'
 
-    Octopush.get(octopush_url + "/run");
-
     # getStatus   
     sleep 50
     url = octopush_url + "/status/#{job_id}"
     json_response = Octopush.get(url)
     json_response.body.should include 'PENDING_TESTS'
-
-    Octopush.get(octopush_url + "/run");
 
     # report TEST OK    
     sleep 5
@@ -45,8 +39,6 @@ describe "Octopush end to end pipeline flow" do
     data = { :body => {:success => true} }
     json_response = Octopush.post(url, data)
     json_response.body.should include 'success'
-
-    Octopush.get(octopush_url + "/run");
 
     # getStatus
     sleep 5   
