8eb50355e430d6d6360cc887498c9068fe290552
nahuel-rosso-olx <nahuel.rosso@olx.com>
2014-05-22 16:58:04 -0300

    INFR-25347 - Fixes and ThirdParty Service

---
+++ src/Services/ThirdParty.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Services;
+
+use Library\HttpRequest;
+
+class ThirdParty
+{
+
+    private $_preDeployUrl;
+    private $_security;
+    private $_httpRequest;
+    private $_log;
+
+    public function __construct($config, HttpRequest $httpRequest, $log)
+    {
+        $this->_preDeployUrl = $config['thirdparty']['pre_deploy'];
+        $this->_security = $config['thirdparty']['security'];
+        $this->_log = $log;
+        $this->_httpRequest = $httpRequest;
+        $this->_log->addInfo("ThirdParty instance created");
+    }
+
+    public function TagReporterPreDeploy($repo, $version, $action = 'deploy')
+    {
+        $urlParams = array(
+            'repo' => $repo,
+            'version' => $version,
+            'action' => $action
+        );
+        
+        return $this->_callToPreDeploy($urlParams);
+    }
+    
+    
+    private function _callToPreDeploy($params = array()) {
+        $url = $this->_preDeployUrl . http_build_query($params);
+        return json_decode(file_get_contents($url));
+    }
+
+}
