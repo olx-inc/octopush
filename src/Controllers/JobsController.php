d9831fcf16fc00fa02bd1bab17615f9c7f38baf6
diegog <diegog@olx.com>
2014-05-26 12:17:46 -0300

    decode/encode ticket url

--- src/Controllers/JobsController.php
+++ src/Controllers/JobsController.php
@@ -132,7 +132,7 @@ class JobsController
                 $job->setUser($this->_app['user']->getEmail());
                 
                 if($ticket){
-                    $job->setTicket(urldecode($ticket));
+                    $job->setTicket($ticket);
                     $job->moveStatusTo(JobStatus::QUEUED_FOR_LIVE);
                 } else {
                     $job->movestatusTo(JobStatus::GO_LIVE_FAILED);
